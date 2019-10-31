ddf3a6fc1a05bfa4eb5f0ba51d89acc68aac7571
Sasa Schwartz <seschwartz8@gmail.com>
2019-10-31 13:25:58 -0700

    added more methods up to iPsForDays

--- LogAnalyzer.java
+++ LogAnalyzer.java
@@ -1,9 +1,6 @@
 
 /**
- * Write a description of class LogAnalyzer here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Sarah Schwartz (code for WebLogParser provided by Duke)
  */
 
 import java.util.*;
@@ -74,6 +71,67 @@ public class LogAnalyzer
              } 
          }
          return uniqueIPsInRange.size();
+     }
+     
+     public HashMap<String,Integer> countVisitsPerIP() {
+         // SUMMARY: returns HashMap of IP addresses to their counts of visits
+         HashMap<String,Integer> visitsPerIP = new HashMap<String,Integer>();
+         for (LogEntry le: records) {
+             String address = le.getIpAddress();
+             if (!visitsPerIP.containsKey(address)) {
+                 visitsPerIP.put(address, 1);
+             } else {
+                 visitsPerIP.put(address, visitsPerIP.get(address) + 1);
+             }
+         }
+         return visitsPerIP;
+     }
+     
+     public int mostVisitsByIP(HashMap<String,Integer> visitsPerIP){
+         // SUMMARY: returns maximum number of visits to website by single IP address
+         int maxVisits = 0;
+         for (Integer visits : visitsPerIP.values()){
+             if (visits > maxVisits) {
+                 maxVisits = visits;
+             }
+         }
+         return maxVisits;
+     }
+     
+     public ArrayList<String> iPsMostVisits(HashMap<String,Integer> visitsPerIP){
+         // SUMMARY: returns IP addresses that visited website max number of times
+         ArrayList<String> mostVisitsIPs = new ArrayList<String>();
+         int max = mostVisitsByIP(visitsPerIP);
+         for (String address : visitsPerIP.keySet()){
+             if (visitsPerIP.get(address).equals(max)){
+                 mostVisitsIPs.add(address);
+             }
+         }
+         return mostVisitsIPs;
+     }
+     
+     public HashMap<String,ArrayList<String>> iPsForDays(){
+         // SUMMARY: returns HashMap mapping dates to the IPs that visited on that day (including repeat visits)
+         HashMap<String,ArrayList<String>> iPsEachDay = new HashMap<String,ArrayList<String>>();
+         for (LogEntry le : records){
+             String fullDate = le.getAccessTime().toString();
+             // Just take the Day/Month/Date (e.g. "Wed Sep 04") part of the date
+             String date = fullDate.substring(0, 11);
+             String address = le.getIpAddress();
+             if (!iPsEachDay.containsKey(date)){
+                 // If the map doesn't contain a date yet, add it and start a list of addresses
+                 ArrayList<String> firstAddresses = new ArrayList<String>();
+                 firstAddresses.add(address);
+                 iPsEachDay.put(date, firstAddresses);
+             } else {
+                 // If the map does contain the date, add the current address to the list of addresses at that date
+                 System.out.println("map contained " + date);
+                 ArrayList<String> currentAddresses = iPsEachDay.get(date);
+                 currentAddresses.add(address);
+                 iPsEachDay.replace(date, currentAddresses);
+             }
+         }
+         return iPsEachDay;
      }
         
      public void printAll() {
--- Tester.java
+++ Tester.java
@@ -22,7 +22,7 @@ public class Tester
         LogAnalyzer analyzer = new LogAnalyzer();
         analyzer.readFile("short-test_log");
         analyzer.printAll();
-        // Number of uinique IP addresses
+        // Print number of uinique IP addresses
         LogAnalyzer analyzer2 = new LogAnalyzer();
         analyzer2.readFile("short-test_log");
         int uniqueIps = analyzer2.countUniqueIps();
@@ -40,5 +40,25 @@ public class Tester
         LogAnalyzer analyzer5 = new LogAnalyzer();
         analyzer5.readFile("short-test_log");
         System.out.println(analyzer5.countUniqueIPsInRange(200,299));
+        // Prints number of visits for each IP address
+        LogAnalyzer analyzer6 = new LogAnalyzer();
+        analyzer6.readFile("short-test_log");
+        HashMap<String,Integer> counts = analyzer6.countVisitsPerIP();
+        System.out.println(counts);
+        // Prints max number of visits by single IP address
+        LogAnalyzer analyzer7 = new LogAnalyzer();
+        analyzer7.readFile("weblog3-short_log");
+        int maxVisits = analyzer7.mostVisitsByIP(analyzer7.countVisitsPerIP());
+        System.out.println(maxVisits);
+        // Prints list of addresses that visited website max times
+        LogAnalyzer analyzer8 = new LogAnalyzer();
+        analyzer8.readFile("weblog3-short_log");
+        ArrayList<String> maxIPs = analyzer8.iPsMostVisits(analyzer8.countVisitsPerIP());
+        System.out.println(maxIPs);
+        // Prints HashMap with addresses that visited a site each day
+        LogAnalyzer analyzer9 = new LogAnalyzer();
+        analyzer9.readFile("weblog3-short_log");
+        HashMap<String,ArrayList<String>> iPsEachDay = analyzer9.iPsForDays();
+        System.out.println(iPsEachDay);
     }
 }
